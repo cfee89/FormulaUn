4fc6ca65f72f3282346a4b3620f964eedf07dff5
Cfee <cfee89@gmail.com>
2022-03-23 18:23:44 -0400

    updated slack API to match post condition as listed in docs

--- routes/slack.js
+++ routes/slack.js
@@ -2,8 +2,8 @@ var ergastApiService = require('../services/ergastApiService');
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', async function(req, res, next) {
+/* POST slack inputs. */
+router.post('/', async function(req, res, next) {
 //  let driverResult = await ergastApiService.getDrivers();
 console.log(req);
 });
