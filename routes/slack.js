24a749a8ee2cf42da2000952bf62e95b19985b2e
Cfee <cfee89@gmail.com>
2022-03-23 20:36:56 -0400

    stringifying result

--- routes/slack.js
+++ routes/slack.js
@@ -10,7 +10,7 @@ router.post('/', async function(req, res, next) {
 
   result = slackParser.parseSlackCommand(req);
   message = slackMessageBuilder.buildDriversListMessage(result);
-  res.json(result);
+  res.json({ 'text': JSON.stringify(result) });
 });
 
 module.exports = router;
