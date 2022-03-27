9a3f5a35bf7c9a454f6794e39a6546cb18569c0e
Cfee <cfee89@gmail.com>
2022-03-27 16:14:46 -0400

    using +=

--- services/slackMessageBuilder.js
+++ services/slackMessageBuilder.js
@@ -10,7 +10,7 @@ class SlackMessageBuilder {
         driversList.forEach((driver)=>{
             let driverString = `${driver.permanentNumber} ${driver.code} ${driver.familyName} \n`
             console.log(driverString);
-            allDrivers.concat(driverString);
+            allDrivers+=driverString;
         });
         
         console.log("The driver string is: " + JSON.stringify(allDrivers));
