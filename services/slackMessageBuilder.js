610871ddd6edcd582f6737170314b6f6a0d4208a
Cfee <cfee89@gmail.com>
2022-03-27 15:19:30 -0400

    updated message for all drivers

--- services/slackMessageBuilder.js
+++ services/slackMessageBuilder.js
@@ -4,13 +4,23 @@ class SlackMessageBuilder {
     }
 
     buildDriversListMessage(driversList) {
-        let message = {
-                'response_type': 'in_channel',
+        let allDrivers = '';
+        for(driver in driversList) {
+            let driverString = `${driver.permanentNumber} ${driver.code} ${driver.familyName} \n`;
+            allDrivers.append(driverString);
+        }
+        
+        let message = { 
+            'response_type': 'in_channel',
+            'blocks': [
                 "text": {
                     "type":"mrkdwn",
-                    "text": JSON.stringify(driversList[0])
+                    "text": allDrivers
                 }
-            }
+            ]
+        }
+        
+        console.log('Drivers message is: ' + message);
         return message;
     }
 }
