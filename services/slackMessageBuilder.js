da9ac8c17ec364fd38497bdb09b69e7733993d2c
Cfee <cfee89@gmail.com>
2022-03-27 18:03:16 -0400

    more spaces

--- services/slackMessageBuilder.js
+++ services/slackMessageBuilder.js
@@ -69,17 +69,17 @@ class SlackMessageBuilder {
             case '3':
                 return ':third_place_medal:';
             case '4':
-                return '4  ';
+                return ' 4  ';
             case '5':
-                return '5  ';
+                return ' 5  ';
             case '6':
-                return '6  ';
+                return ' 6  ';
             case '7':
-                return '7  ';
+                return ' 7  ';
             case '8':
-                return '8  ';
+                return ' 8  ';
             case '9':
-                return '9  ';             
+                return ' 9  ';             
             default:
                 return position;
         }
